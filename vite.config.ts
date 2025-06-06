67f1d4569e4180f2a3b02b7c8b56b152f9e5fa4c
Rafael Gava <rafaelgava921@gmail.com>
2025-06-06 10:56:41 -0300

    feat: update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,33 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [
     react(),
   ],
+  root: '.',
+	base: '/',
+	build: {
+		chunkSizeWarningLimit: 1024,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor'
+					}
+				},
+			},
+		},
+  },
+  server: {
+		port: 5173,
+		strictPort: true,
+		host: true
+	},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) {
-              return 'react-vendor'; 
-            }
-            if (id.includes('lodash')) {
-              return 'lodash-vendor'; 
-            }
-           
-            return 'vendor'; 
-          }
-        }
-      }
-    }
-  }
 }));
