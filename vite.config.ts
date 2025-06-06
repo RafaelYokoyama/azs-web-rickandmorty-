eed249e099937589246015449389acfdd3caf038
Rafael Gava <rafaelgava921@gmail.com>
2025-06-06 10:06:05 -0300

    chore: initial project setup

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'react-vendor'; 
+            }
+            if (id.includes('lodash')) {
+              return 'lodash-vendor'; 
+            }
+           
+            return 'vendor'; 
+          }
+        }
+      }
+    }
+  }
+}));
